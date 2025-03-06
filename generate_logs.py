95a81b3be1c80cb47efb4661f590950e78e836a8
ygoncloud <64839630+ygoncloud@users.noreply.github.com>
2025-03-06 09:05:33 +0700

    updated train model and logs.py

--- generate_logs.py
+++ generate_logs.py
@@ -5,6 +5,6 @@ data = {"timestamp": ["2024-02-01 12:00", "2024-02-01 12:05"], "event": ["login"
 df = pd.DataFrame(data)
 
 # Simpan file logs.csv
-df.to_csv("/ml/logs.csv", index=False)
+df.to_csv("logs.csv", index=False)
 print("logs.csv created!")
 
