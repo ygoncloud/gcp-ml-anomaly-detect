dc8ba9aea9ea270b27f22a74043f3c368a1380af
ygoncloud <64839630+ygoncloud@users.noreply.github.com>
2025-03-06 09:13:36 +0700

    fix logs.py

--- generate_logs.py
+++ generate_logs.py
@@ -5,6 +5,6 @@ data = {"timestamp": ["2024-02-01 12:00", "2024-02-01 12:05"], "event": ["login"
 df = pd.DataFrame(data)
 
 # Simpan file logs.csv
-log_data = df.to_csv("logs.csv", header=none)
+log_data = df.to_csv("logs.csv", header=None)
 print(log_data())
 
